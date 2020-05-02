eb3811274e2a3236d0efa72508f5e479ab12b600
Alan Baines <abaines@vt.edu>
2020-05-01 23:08:47 -0400

    improve setting tooltips and default
    https://mods.factorio.com/mod/resourceMarker/discussion/5e33bbb778fa0a000da5a1f3

--- settings.lua
+++ settings.lua
@@ -22,7 +22,7 @@ data:extend({
 	{
 		type = "int-setting",
 		name = "resourcemarker-starting-radius-to-generate",
-		default_value = 75,
+		default_value = 40,
 		minimum_value = 0,
 		setting_type = "runtime-global",
 	},

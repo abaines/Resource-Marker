6c1f4fe83532cdb0495bb6bafbbf8a0bb13a273f
Alan Baines <abaines.vt+github@gmail.com>
2024-10-25 09:31:44 -0400

    WIP convert global to storage
    https://discord.com/channels/1214952937613295676/1281881163702730763/1299143136022368317

--- global_lib.lua
+++ global_lib.lua
@@ -11,38 +11,40 @@ end
 
 local M = {}
 
--- lua global
-local _ICON_TYPES_ = "iconTypes" -- global root key
+-- lua storage
+local _ICON_TYPES_ = "iconTypes" -- storage root key
 
-local _RESOURCE_MAP_ = "resourceMap" -- global root key
+local _RESOURCE_MAP_ = "resourceMap" -- storage root key
 
 function M.getGlobalMapLocationData(surface, x, y)
 	local name = surface.name
 
-	global[_RESOURCE_MAP_] = global[_RESOURCE_MAP_] or {}
-	global[_RESOURCE_MAP_][name] = global[_RESOURCE_MAP_][name] or {}
-	global[_RESOURCE_MAP_][name][x] = global[_RESOURCE_MAP_][name][x] or {}
-	global[_RESOURCE_MAP_][name][x][y] = global[_RESOURCE_MAP_][name][x][y] or {}
-	global[_RESOURCE_MAP_][name][x][y].resources = global[_RESOURCE_MAP_][name][x][y].resources or {}
-	global[_RESOURCE_MAP_][name][x][y].forces = global[_RESOURCE_MAP_][name][x][y].forces or {}
+	storage[_RESOURCE_MAP_] = storage[_RESOURCE_MAP_] or {}
+	storage[_RESOURCE_MAP_][name] = storage[_RESOURCE_MAP_][name] or {}
+	storage[_RESOURCE_MAP_][name][x] = storage[_RESOURCE_MAP_][name][x] or {}
+	storage[_RESOURCE_MAP_][name][x][y] = storage[_RESOURCE_MAP_][name][x][y] or {}
+	storage[_RESOURCE_MAP_][name][x][y].resources = storage[_RESOURCE_MAP_][name][x][y].resources or {}
+	storage[_RESOURCE_MAP_][name][x][y].forces = storage[_RESOURCE_MAP_][name][x][y].forces or {}
 
-	return global[_RESOURCE_MAP_][name][x][y]
+	return storage[_RESOURCE_MAP_][name][x][y]
 end
 
+-- https://discord.com/channels/1214952937613295676/1281881163702730763/1299143136022368317
+
 function M.calculateIconTypes()
-	global[_ICON_TYPES_] = {} -- global root key
+	storage[_ICON_TYPES_] = {} -- storage root key
 	for key, _ in pairs(game.virtual_signal_prototypes) do
-		global[_ICON_TYPES_][key] = "virtual"
+		storage[_ICON_TYPES_][key] = "virtual"
 	end
 	for key, _ in pairs(game.item_prototypes) do
-		global[_ICON_TYPES_][key] = "item"
+		storage[_ICON_TYPES_][key] = "item"
 	end
 	for key, _ in pairs(game.fluid_prototypes) do
-		global[_ICON_TYPES_][key] = "fluid"
+		storage[_ICON_TYPES_][key] = "fluid"
 	end
 
 	local resourcePrototypes = game.get_filtered_entity_prototypes({{filter = "type", type = "resource"}})
-	global.aliases = {} -- global root key
+	storage.aliases = {} -- storage root key
 
 	for name, value in pairs(resourcePrototypes) do
 		local products = value.mineable_properties.products or {}
@@ -54,28 +56,28 @@ function M.calculateIconTypes()
 		-- log(sb( products ))
 
 		for _, product in pairs(products) do
-			if global[_ICON_TYPES_][product.name] then
-				global.aliases[name] = product.name
-				global.aliases[i18n.resource(name)] = product.name
+			if storage[_ICON_TYPES_][product.name] then
+				storage.aliases[name] = product.name
+				storage.aliases[i18n.resource(name)] = product.name
 			end
 		end
 	end
-	log("global.aliases:\n" .. sbs(global.aliases))
+	log("storage.aliases:\n" .. sbs(storage.aliases))
 end
 
 function M.clearGlobalResourceMapData()
-	global[_RESOURCE_MAP_] = {}
+	storage[_RESOURCE_MAP_] = {}
 end
 
 function M.logIconTypes()
-	-- global root keys
+	-- storage root keys
 	-- _RESOURCE_MAP_
 	-- _ICON_TYPES_
 	-- aliases
 
 	local function _logIconTypes(_type)
 		local list = {}
-		for key, value in pairs(global[_ICON_TYPES_]) do
+		for key, value in pairs(storage[_ICON_TYPES_]) do
 			if value == _type then
 				table.insert(list, key)
 			end
@@ -88,36 +90,36 @@ function M.logIconTypes()
 	_logIconTypes("item")
 	_logIconTypes("fluid")
 
-	log("global.aliases\n" .. sb(global.aliases))
+	log("storage.aliases\n" .. sb(storage.aliases))
 end
 
--- lua global
+-- lua storage
 local loggedMissingResources = {}
 
 function M.calculateSignalID(resource)
 
 	local signalID = {type = "virtual", name = "signal-dot"}
 
-	local resourceIcon = global.aliases[resource] -- global root key
+	local resourceIcon = storage.aliases[resource] -- storage root key
 	if not resourceIcon then
 		log("Warning: Missing resource icon alias: " .. tostring(resource))
-		log("global.aliases:\n" .. sbs(global.aliases))
-		local global_aliases_size = table_size(global.aliases)
-		if 0 == global_aliases_size then
+		log("storage.aliases:\n" .. sbs(storage.aliases))
+		local storage_aliases_size = table_size(storage.aliases)
+		if 0 == storage_aliases_size then
 			local red = {1, 0, 0}
-			game.print({"errors.global-aliases-missing"}, red)
+			game.print({"errors.storage-aliases-missing"}, red)
 			game.print({"errors.rebuild-data-structure"}, red)
 			M.calculateIconTypes()
-			local global_aliases_size = table_size(global.aliases) -- luacheck: ignore 421
-			if 0 == global_aliases_size then
+			local storage_aliases_size = table_size(storage.aliases) -- luacheck: ignore 421
+			if 0 == storage_aliases_size then
 				game.print({"errors.unable-to-repair"}, red)
-				error("Unable to repair `global.aliases` data structure.")
+				error("Unable to repair `storage.aliases` data structure.")
 			else
 				game.print({"errors.repair-complete"}, red)
 			end
 		end
-	elseif global[_ICON_TYPES_][resourceIcon] then
-		signalID.type = global[_ICON_TYPES_][resourceIcon]
+	elseif storage[_ICON_TYPES_][resourceIcon] then
+		signalID.type = storage[_ICON_TYPES_][resourceIcon]
 		signalID.name = resourceIcon
 	elseif not loggedMissingResources[resourceIcon] then
 		log("Warning: Missing icon type: " .. resourceIcon)

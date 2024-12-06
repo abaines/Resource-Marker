ca736debe22903404212efaa9ce17343a0520a14
Alan Baines <abaines.vt+github@gmail.com>
2024-12-06 18:14:11 -0500

    add logic for getting server language file data

--- english_lib.lua
+++ english_lib.lua
@@ -15,10 +15,34 @@ local M = {}
 -- lua global
 local englishMissingSpamGuard = {}
 
+local function get_global_map_tag_language()
+	return settings.startup["global-map-tag-language"].value
+end
+
+
+local function get_language_server_file()
+	local global_map_tag_language = get_global_map_tag_language()
+
+	if global_map_tag_language=="en" then
+		return english
+	elseif global_map_tag_language=="ru" then
+		return russian
+	else
+		local msg = "Unknown language!" .. str(global_map_tag_language)
+		log(msg)
+		error(msg)
+	end
+end
+
+
 function M.resource(resource)
+	local global_map_tag_language = get_global_map_tag_language()
+	log(global_map_tag_language)
+	local i18n_file_data = get_language_server_file()
+
 	local i18n = english[resource]
 	local i18n_russian = russian[resource]
-	
+
 	if not i18n and not englishMissingSpamGuard[resource] then
 		local msg = "The english.lua table missing `" .. resource .. "`"
 		log(msg)
